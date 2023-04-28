cb6cc6735f4ffff8bef0beeb4d187d1f651fd576
Persune <gumball2415@gmail.com>
2023-04-28 12:43:10 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Palette Generator
+
+yet another NES palette generator, in Python
+![complete NES palette](docs\palette.png)
+![waveform preview](docs\waveform.png)
+
+## Requirements
+
+This script requires `colour` and `numpy` for color decoding.
+This script requires `matplotlib.pyplot` for previewing
+
+## Usage
+```python
+usage: palgen-persune.py [-h] [-o OUTPUT] [-e] [-v] [-d]
+
+yet another NES palette generator
+
+options:
+  -h, --help            show this help message and exit
+  -o OUTPUT, --output OUTPUT
+                        .pal file output
+  -e, --emphasis        add emphasis entries
+  -v, --verbose         look at waveforms
+  -d, --debug           debug messages
+
+version 0.0.0
+```
+
+## Licence
+
+This work is licensed under the MIT-0 license.
+Copyright (C) Persune 2023.
---
+++ palgen-persune.py
@@ -0,0 +1,192 @@
+# palgen NES
+# Copyright (C) 2023 Persune
+# inspired by PalGen, Copyright (C) 2018 DragWx <https://github.com/DragWx>
+# testing out the concepts from https://www.nesdev.org/wiki/NTSC_video#Composite_decoding
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this
+# software and associated documentation files (the "Software"), to deal in the Software
+# without restriction, including without limitation the rights to use, copy, modify,
+# merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+# PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+import argparse
+import sys
+import os
+import numpy as np
+import colour
+import matplotlib.pyplot as plt
+
+
+parser=argparse.ArgumentParser(
+    description="yet another NES palette generator",
+    epilog="version 0.0.0")
+parser.add_argument("-o", "--output", type=str, help=".pal file output")
+parser.add_argument("-e", "--emphasis", action="store_true", help="add emphasis entries")
+parser.add_argument("-v", "--verbose", action="store_true", help="look at waveforms")
+parser.add_argument("-d", "--debug", action="store_true", help="debug messages")
+
+args = parser.parse_args()
+
+# voltage highs and lows
+# signal[4][2][2] $0x-$3x, $x0/$xD, no emphasis/emphasis
+lidnariq_NESCPU07_2C02G_NES001_signal = np.array([
+    [
+        [ 0.616, 0.500 ],
+        [ 0.228, 0.192 ]
+    ],
+    [
+        [ 0.840, 0.676 ],
+        [ 0.312, 0.256 ]
+    ],
+    [
+        [ 1.100, 0.896 ],
+        [ 0.552, 0.448 ]
+    ],
+    [
+        [ 1.100, 0.896 ],
+        [ 0.880, 0.712 ]
+    ]
+], np.float64)
+
+# signal buffer normalization
+signal_black_point = lidnariq_NESCPU07_2C02G_NES001_signal[1, 1, 0]
+signal_white_point = lidnariq_NESCPU07_2C02G_NES001_signal[3, 0, 0]
+amplification_factor = 1/(signal_white_point - signal_black_point)
+
+# 111111------
+# 22222------2
+# 3333------33
+# 444------444
+# 55------5555
+# 6------66666
+# ------777777
+# -----888888-
+# ----999999--
+# ---AAAAAA---
+# --BBBBBB----
+# -CCCCCC-----
+# signal buffer for decoding
+voltage_buffer = np.empty([12], np.float64)
+
+# final 24-bit RGB palette
+PaletteColors = np.empty([8,4,16,3], np.uint8)
+
+# decoded YUV buffer,
+YUV_buffer = np.empty([8,4,16,3], np.float64)
+
+# decoded RGB buffer
+RGB_buffer = np.empty([8,4,16,3], np.float64)
+
+# fix issue with colors
+offset = 2
+emphasis_offset = 1
+colorburst = 8
+colorburst_offset = colorburst - 7
+
+for emphasis in range(8):
+    # emphasis bitmask
+    emphasis_wave = 0
+    if bool(emphasis & 0b001):		# tint R; aligned to color phase C
+        emphasis_wave |= 0b000001111110;
+    if bool(emphasis & 0b010):		# tint G; aligned to color phase 4
+        emphasis_wave |= 0b111000000111;
+    if bool(emphasis & 0b100):		# tint B; aligned to color phase 8
+        emphasis_wave |= 0b011111100000;
+
+    for luma in range(4):
+        for hue in range(16):
+            # encode voltages into composite waveform
+            for wave_phase in range(12):
+                # 0 = waveform high; 1 = waveform low
+                n_wave_level = 0
+                # 1 = emphasis activate
+                emphasis_level = int(bool(emphasis_wave & (1 << ((wave_phase - hue + emphasis_offset) % 12))))
+
+                if (wave_phase >= 6): n_wave_level = 1
+                    
+                # rows $x0 amd $xD
+                if (hue == 0x00): n_wave_level = 0
+                if (hue == 0x0D): n_wave_level = 1
+                
+                #rows $xE-$xF
+                if (hue >= 0x0E):
+                    voltage_buffer[wave_phase] = lidnariq_NESCPU07_2C02G_NES001_signal[1, 1, 0]
+                else:
+                    voltage_buffer[(wave_phase - hue + offset) % 12] = lidnariq_NESCPU07_2C02G_NES001_signal[luma, n_wave_level, emphasis_level]
+            
+            # normalize voltage
+            voltage_buffer -= signal_black_point
+            voltage_buffer *= amplification_factor
+            
+            # decode voltage buffer to YUV
+            YUV_buffer[emphasis, luma, hue, 0] = np.average(voltage_buffer)
+            
+            UV_buffer = np.empty([12], np.float64)
+            
+            for t in range(12):
+                UV_buffer[t] = voltage_buffer[t] * np.sin(2*np.pi*(1/12)*(t+colorburst_offset))
+            YUV_buffer[emphasis, luma, hue, 1] = np.average(UV_buffer)
+            
+            for t in range(12):
+                UV_buffer[t] = voltage_buffer[t] * np.cos(2*np.pi*(1/12)*(t+colorburst_offset))
+            YUV_buffer[emphasis, luma, hue, 2] = np.average(UV_buffer)
+
+
+            # decode YUV to RGB
+            # derived from the NTSC base matrix of luminance and color-difference
+            RGB_buffer[emphasis, luma, hue, 0] = YUV_buffer[emphasis, luma, hue, 0] + YUV_buffer[emphasis, luma, hue, 2]*1.14
+            RGB_buffer[emphasis, luma, hue, 1] = YUV_buffer[emphasis, luma, hue, 0] - YUV_buffer[emphasis, luma, hue, 1]*0.394242 - YUV_buffer[emphasis, luma, hue, 2]*0.580681
+            RGB_buffer[emphasis, luma, hue, 2] = YUV_buffer[emphasis, luma, hue, 0] + YUV_buffer[emphasis, luma, hue, 1]*2.03
+            
+            # clip RGB to 0.0-1.0
+            for i in range(3):
+                RGB_buffer[emphasis, luma, hue, i] = max(0, min(1, 
+                    RGB_buffer[emphasis, luma, hue, i]))
+
+            # convert RGB to display output
+            # TODO: color primaries transform from one profile to another
+            PaletteColors[emphasis, luma, hue] = RGB_buffer[emphasis, luma, hue] * 0xFF
+            
+            if (args.debug):
+                print("{0:02X} emphasis {1:03b}".format((luma<<4 | hue), emphasis) + "\n" + str(voltage_buffer))
+            if (args.verbose):
+                plt.title("{0:02X} emphasis pattern {1:012b}".format((luma<<4 | hue), emphasis_wave))
+                x = np.arange(0,12)
+                y = voltage_buffer
+
+                plt.xlabel("Sample count")
+                plt.ylabel("Voltage")
+                plt.step(x, y, linewidth=0.7)
+                plt.tight_layout()
+                plt.draw()
+                plt.show()
+
+    if not (args.emphasis):
+        print("emphasis skipped")
+        break
+
+# crop non-emphasis colors if not enabled
+if (args.emphasis):
+    PaletteColorsFinal = np.reshape(PaletteColors,(32, 16, 3))
+else:
+    PaletteColorsFinal = PaletteColors[0]
+
+if (type(args.output) != type(None)):
+    with open(args.output, mode="wb") as PaletteFile:
+        PaletteFile.write(PaletteColorsFinal)
+
+# figure plotting for palette preview
+# TODO: add more graphs, including CIE graph
+plt.title("palette")
+plt.imshow(PaletteColorsFinal)
+plt.tight_layout()
+plt.draw()
+plt.show()
