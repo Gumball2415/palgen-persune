5ca194e508806eb9d583664bb67b22148be54e06
Persune <gumball2415@gmail.com>
2023-11-15 01:59:54 +0800

    Fix GV-USB2 NTSC M_J whitepoint

--- make palettes.bat
+++ make palettes.bat
@@ -1,8 +1,8 @@
-palgen-persune.py --skip-plot -aps 5 -ela 0.01429 -phs -5.0 -blp 0.05357142857142857142857142857143-rfc "NTSC (1953)" -e -o docs/example_palettes/2C02G_phs_aps_ela_NTSC-1953.pal 
+palgen-persune.py --skip-plot -aps 5 -ela 0.01429 -phs -5.0 -blp 0.05357142857142857142857142857143 -rfc "NTSC (1953)" -e -o docs/example_palettes/2C02G_phs_aps_ela_NTSC-1953.pal 
 palgen-persune.py --skip-plot -aps 5 -ela 0.01429 -phs -5.0 -rpr 0.618 0.350 -rpg 0.280 0.605 -rpb 0.152 0.063 -rpw 0.28314501 0.29711289 -e -o docs/example_palettes/2C02G_phs_aps_ela_NTSC-J.pal
 palgen-persune.py --skip-plot -aps 5 -ela 0.01429 -phs -5.0 -blp 0.05357142857142857142857142857143 -e -o docs/example_palettes/2C02G_phs_aps_ela_NTSC.pal
 palgen-persune.py --skip-plot -aps 5 -ela 0.01429 -e -hue 7.5 -sat -0.4 -blp 0.05357142857142857142857142857143 -o docs/example_palettes/2C02-2C07_aps_ela_persune_neutral.pal
 palgen-persune.py --skip-plot -aps 5 -ela 0.01429 -e -hue -2.5 -sat -0.1  -o docs/example_palettes/2C02G_aps_ela_NTSC_persune_tink.pal
 palgen-persune.py --skip-plot -aps 5 -ela 0.01429 -e -hue 0 -whp 0.76785714285714285714285714285714 -blp 0.05357142857142857142857142857143 -o docs/example_palettes/2C02G_aps_ela_NTSC_persune_GVUSB2_NTSC_M.pal
-palgen-persune.py --skip-plot -aps 5 -ela 0.01429 -e -hue 0 -whp 0.76785714285714285714285714285714 -o docs/example_palettes/2C02G_aps_ela_NTSC_persune_GVUSB2_NTSC_MJ.pal
+palgen-persune.py --skip-plot -aps 5 -ela 0.01429 -e -hue 0 -whp 0.71428571428571428571428571428571 -o docs/example_palettes/2C02G_aps_ela_NTSC_persune_GVUSB2_NTSC_MJ.pal
 palgen-persune.py --skip-plot -aps 5 -ela 0.01429 -pal -cbr 7 -blp 0.05357142857142857142857142857143 -e -o docs/example_palettes/2C07_aps_ela_PAL.pal
