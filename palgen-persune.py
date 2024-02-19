8600a7b8160deb4a8c4e78d67a07330f47b27159
Persune <gumball2415@gmail.com>
2024-02-19 19:15:56 +0800

    Suppress warnings

--- palgen-persune.py
+++ palgen-persune.py
@@ -292,6 +292,7 @@ def palette_plot(RGB_buffer,
 
     import matplotlib.pyplot as plt
     import matplotlib.gridspec as gridspec
+    import colour.plotting.diagrams
 
     RGB_sub = RGB_buffer
     RGB_sub_raw = RGB_uncorrected
@@ -995,166 +996,169 @@ def main(argv=None):
     # special thanks to _aitchFactor for pointing out that colour-science has
     # chromatic adaptation functions!
 
-    # reference color profile colorspace
-    s_colorspace = colour.RGB_Colourspace(
-        colour.RGB_COLOURSPACES[args.reference_colorspace].name,
-        colour.RGB_COLOURSPACES[args.reference_colorspace].primaries,
-        colour.RGB_COLOURSPACES[args.reference_colorspace].whitepoint)
+    # suppress warnings for colour-science
+    with colour.utilities.suppress_warnings(*[True] * 4):
+        # reference color profile colorspace
+        s_colorspace = colour.RGB_Colourspace(
+            colour.RGB_COLOURSPACES[args.reference_colorspace].name,
+            colour.RGB_COLOURSPACES[args.reference_colorspace].primaries,
+            colour.RGB_COLOURSPACES[args.reference_colorspace].whitepoint)
 
-    if (args.reference_primaries_r is not None
-        and args.reference_primaries_g is not None
-        and args.reference_primaries_b is not None):
-        s_colorspace.name = "custom primaries"
-        s_colorspace.primaries = np.array([
-            args.reference_primaries_r,
-            args.reference_primaries_g,
-            args.reference_primaries_b
-        ])
-    else:
-        s_colorspace.name = colour.RGB_COLOURSPACES[args.reference_colorspace].name
-        s_colorspace.primaries = colour.RGB_COLOURSPACES[args.reference_colorspace].primaries
+        if (args.reference_primaries_r is not None
+            and args.reference_primaries_g is not None
+            and args.reference_primaries_b is not None):
+            s_colorspace.name = "custom primaries"
+            s_colorspace.primaries = np.array([
+                args.reference_primaries_r,
+                args.reference_primaries_g,
+                args.reference_primaries_b
+            ])
+        else:
+            s_colorspace.name = colour.RGB_COLOURSPACES[args.reference_colorspace].name
+            s_colorspace.primaries = colour.RGB_COLOURSPACES[args.reference_colorspace].primaries
 
-    if (args.reference_primaries_w is not None):
-        s_colorspace.whitepoint = args.reference_primaries_w
-        s_colorspace.whitepoint_name = "custom whitepoint"
-    else:
-        s_colorspace.whitepoint_name = colour.RGB_COLOURSPACES[args.reference_colorspace].whitepoint_name
-        s_colorspace.whitepoint = colour.RGB_COLOURSPACES[args.reference_colorspace].whitepoint
+        if (args.reference_primaries_w is not None):
+            s_colorspace.whitepoint = args.reference_primaries_w
+            s_colorspace.whitepoint_name = "custom whitepoint"
+        else:
+            s_colorspace.whitepoint_name = colour.RGB_COLOURSPACES[args.reference_colorspace].whitepoint_name
+            s_colorspace.whitepoint = colour.RGB_COLOURSPACES[args.reference_colorspace].whitepoint
 
-    # display color profile colorspace
-    t_colorspace = colour.RGB_Colourspace(
-        colour.RGB_COLOURSPACES[args.display_colorspace].name,
-        colour.RGB_COLOURSPACES[args.display_colorspace].primaries,
-        colour.RGB_COLOURSPACES[args.display_colorspace].whitepoint)
+        # display color profile colorspace
+        t_colorspace = colour.RGB_Colourspace(
+            colour.RGB_COLOURSPACES[args.display_colorspace].name,
+            colour.RGB_COLOURSPACES[args.display_colorspace].primaries,
+            colour.RGB_COLOURSPACES[args.display_colorspace].whitepoint)
 
-    if (args.display_primaries_r is not None
-        and args.display_primaries_g is not None
-        and args.display_primaries_b is not None): 
-        t_colorspace.name = "custom primaries"
-        t_colorspace.primaries = np.array([
-            args.display_primaries_r,
-            args.display_primaries_g,
-            args.display_primaries_b
-        ])
-    else:
-        t_colorspace.name = colour.RGB_COLOURSPACES[args.display_colorspace].name
-        t_colorspace.primaries = colour.RGB_COLOURSPACES[args.display_colorspace].primaries
+        if (args.display_primaries_r is not None
+            and args.display_primaries_g is not None
+            and args.display_primaries_b is not None): 
+            t_colorspace.name = "custom primaries"
+            t_colorspace.primaries = np.array([
+                args.display_primaries_r,
+                args.display_primaries_g,
+                args.display_primaries_b
+            ])
+        else:
+            t_colorspace.name = colour.RGB_COLOURSPACES[args.display_colorspace].name
+            t_colorspace.primaries = colour.RGB_COLOURSPACES[args.display_colorspace].primaries
 
-    if (args.display_primaries_w is not None):
-        t_colorspace.whitepoint = args.display_primaries_w
-        t_colorspace.whitepoint_name = "custom whitepoint"
-    else:
-        t_colorspace.whitepoint_name = colour.RGB_COLOURSPACES[args.display_colorspace].whitepoint_name
-        t_colorspace.whitepoint = colour.RGB_COLOURSPACES[args.display_colorspace].whitepoint
+        if (args.display_primaries_w is not None):
+            t_colorspace.whitepoint = args.display_primaries_w
+            t_colorspace.whitepoint_name = "custom whitepoint"
+        else:
+            t_colorspace.whitepoint_name = colour.RGB_COLOURSPACES[args.display_colorspace].whitepoint_name
+            t_colorspace.whitepoint = colour.RGB_COLOURSPACES[args.display_colorspace].whitepoint
 
-    s_colorspace.name = "Reference colorspace: {}".format(s_colorspace.name)
-    t_colorspace.name = "Display colorspace: {}".format(t_colorspace.name)
+        s_colorspace.name = "Reference colorspace: {}".format(s_colorspace.name)
+        t_colorspace.name = "Display colorspace: {}".format(t_colorspace.name)
 
-    # decoded RGB buffer
-    # has to be zero'd out for the normalize function to work
-    RGB_buffer = np.zeros([8,4,16,3], np.float64)
-    signal_black_point = 0
-    signal_white_point = 100
+        # decoded RGB buffer
+        # has to be zero'd out for the normalize function to work
+        RGB_buffer = np.zeros([8,4,16,3], np.float64)
+        signal_black_point = 0
+        signal_white_point = 100
 
-    # generate color!
-    match args.ppu:
-        case "2C03"|"2C04-0000"|"2C04-0001"|"2C04-0002"|"2C04-0003"|"2C04-0004"|"2C05-99":
-            # signal buffer normalization
-            if (args.black_point is not None):
-                signal_black_point = args.black_point
+        # generate color!
+        match args.ppu:
+            case "2C03"|"2C04-0000"|"2C04-0001"|"2C04-0002"|"2C04-0003"|"2C04-0004"|"2C05-99":
+                # signal buffer normalization
+                if (args.black_point is not None):
+                    signal_black_point = args.black_point
 
-            if (args.white_point is not None):
-                signal_white_point = args.white_point
-                
-            # we use RGB_buffer[] as a temporary buffer for YUV
-            RGB_buffer = pixel_codec_rgb(RGB_buffer, args, signal_black_point, signal_white_point)
-        case "2C02"|"2C07":
-            # signal buffer normalization
-            if (args.black_point is not None):
-                signal_black_point = args.black_point
+                if (args.white_point is not None):
+                    signal_white_point = args.white_point
+                    
+                # we use RGB_buffer[] as a temporary buffer for YUV
+                RGB_buffer = pixel_codec_rgb(RGB_buffer, args, signal_black_point, signal_white_point)
+            case "2C02"|"2C07":
+                # signal buffer normalization
+                if (args.black_point is not None):
+                    signal_black_point = args.black_point
 
-            if (args.white_point is not None):
-                signal_white_point = args.white_point
-            else:
-                signal_white_point = 140 * (signal_table_composite[3, 0, 0] - signal_table_composite[1, 1, 0])
-            # we use RGB_buffer[] as a temporary buffer for YUV
-            RGB_buffer = pixel_codec_composite(RGB_buffer, args, signal_black_point, signal_white_point)
+                if (args.white_point is not None):
+                    signal_white_point = args.white_point
+                else:
+                    signal_white_point = 140 * (signal_table_composite[3, 0, 0] - signal_table_composite[1, 1, 0])
 
-    # reshape buffer after encoding
-    if (args.emphasis):
-        RGB_buffer = np.reshape(RGB_buffer,(32, 16, 3))
-    else:
-        RGB_buffer = np.reshape(RGB_buffer,(4, 16, 3))
+                # we use RGB_buffer[] as a temporary buffer for YUV
+                RGB_buffer = pixel_codec_composite(RGB_buffer, args, signal_black_point, signal_white_point)
 
-    # convert back to RGB
-    RGB_buffer = np.einsum('ij,klj->kli', np.linalg.inv(RGB_to_YUV), RGB_buffer, dtype=np.float64)
+        # reshape buffer after encoding
+        if (args.emphasis):
+            RGB_buffer = np.reshape(RGB_buffer,(32, 16, 3))
+        else:
+            RGB_buffer = np.reshape(RGB_buffer,(4, 16, 3))
 
-    # apply black and white points
-    # this also scales the values back roughly within range of 0 to 1
-    RGB_buffer -= signal_black_point
-    RGB_buffer /= (signal_white_point - signal_black_point)
+        # convert back to RGB
+        RGB_buffer = np.einsum('ij,klj->kli', np.linalg.inv(RGB_to_YUV), RGB_buffer, dtype=np.float64)
 
-    # debug: a rough vectorscope plot
-    # NES_SMPTE_plot(RGB_buffer, 0, args, plt)
+        # apply black and white points
+        # this also scales the values back roughly within range of 0 to 1
+        RGB_buffer -= signal_black_point
+        RGB_buffer /= (signal_white_point - signal_black_point)
 
-    # fit RGB within range of 0.0-1.0
-    normalize_RGB(RGB_buffer, args)
+        # debug: a rough vectorscope plot
+        # NES_SMPTE_plot(RGB_buffer, 0, args, plt)
 
-    # preserve uncorrected RGB for color plotting
-    RGB_uncorrected = RGB_buffer
+        # fit RGB within range of 0.0-1.0
+        normalize_RGB(RGB_buffer, args)
 
-    # convert RGB to display output
+        # preserve uncorrected RGB for color plotting
+        RGB_uncorrected = RGB_buffer
 
-    # convert linear signal to linear light, if permitted
-    if (not args.electro_optic_disable):
-        RGB_buffer = colour.oetf_inverse(RGB_buffer, function=args.opto_electronic)
-        RGB_uncorrected = colour.oetf_inverse(RGB_uncorrected, function=args.opto_electronic)
+        # convert RGB to display output
 
-    # transform color primaries
-    if (args.inverse_chromatic_transform):
-        RGB_buffer = colour.RGB_to_RGB(
-            RGB_buffer,
-            t_colorspace,
-            s_colorspace,
-            chromatic_adaptation_transform=args.chromatic_adaptation_transform)
-    else:
-        RGB_buffer = colour.RGB_to_RGB(
-            RGB_buffer,
-            s_colorspace,
-            t_colorspace,
-            chromatic_adaptation_transform=args.chromatic_adaptation_transform)
+        # convert linear signal to linear light, if permitted
+        if (not args.electro_optic_disable):
+            RGB_buffer = colour.oetf_inverse(RGB_buffer, function=args.opto_electronic)
+            RGB_uncorrected = colour.oetf_inverse(RGB_uncorrected, function=args.opto_electronic)
 
-    # convert linear light to linear signal, if permitted
-    if (not args.opto_electronic_disable):
-        RGB_buffer = colour.oetf(RGB_buffer, function=args.opto_electronic)
-        RGB_uncorrected = colour.oetf(RGB_uncorrected, function=args.opto_electronic)
+        # transform color primaries
+        if (args.inverse_chromatic_transform):
+            RGB_buffer = colour.RGB_to_RGB(
+                RGB_buffer,
+                t_colorspace,
+                s_colorspace,
+                chromatic_adaptation_transform=args.chromatic_adaptation_transform)
+        else:
+            RGB_buffer = colour.RGB_to_RGB(
+                RGB_buffer,
+                s_colorspace,
+                t_colorspace,
+                chromatic_adaptation_transform=args.chromatic_adaptation_transform)
 
-    # clip again, the transform may produce values beyond 0-1
-    normalize_RGB(RGB_buffer, args)
-    normalize_RGB(RGB_uncorrected, args)
-    
-    output_format = {
-        ".pal uint8": output_binary_uint8,
-        ".pal double": output_binary_double,
-        ".pal Jasc": output_jasc_pal,
-        ".txt HTML hex": output_html_hex,
-        ".txt MediaWiki": output_mediawiki_table,
-        ".h uint8_t": output_cstyle_table,
-        ".gpl": output_gimp_pal,
-        ".png": output_png
+        # convert linear light to linear signal, if permitted
+        if (not args.opto_electronic_disable):
+            RGB_buffer = colour.oetf(RGB_buffer, function=args.opto_electronic)
+            RGB_uncorrected = colour.oetf(RGB_uncorrected, function=args.opto_electronic)
+
+        # clip again, the transform may produce values beyond 0-1
+        normalize_RGB(RGB_buffer, args)
+        normalize_RGB(RGB_uncorrected, args)
         
-    }
-    if (args.output is not None):
-        output_format[args.file_format](RGB_buffer, args)
-  
+        output_format = {
+            ".pal uint8": output_binary_uint8,
+            ".pal double": output_binary_double,
+            ".pal Jasc": output_jasc_pal,
+            ".txt HTML hex": output_html_hex,
+            ".txt MediaWiki": output_mediawiki_table,
+            ".h uint8_t": output_cstyle_table,
+            ".gpl": output_gimp_pal,
+            ".png": output_png
+            
+        }
+        if (args.output is not None):
+            output_format[args.file_format](RGB_buffer, args)
+      
 
-    if (args.render_img is not None):
-        for emphasis in range(8):
-            palette_plot(RGB_buffer, RGB_uncorrected, emphasis, False, False, (args.render_img is not None), args, s_colorspace, t_colorspace)
-            if not (args.emphasis):
-                break
+        if (args.render_img is not None):
+            for emphasis in range(8):
+                palette_plot(RGB_buffer, RGB_uncorrected, emphasis, False, False, (args.render_img is not None), args, s_colorspace, t_colorspace)
+                if not (args.emphasis):
+                    break
 
-    palette_plot(RGB_buffer, RGB_uncorrected, 0, True, False, (args.render_img is not None), args, s_colorspace, t_colorspace)
+        palette_plot(RGB_buffer, RGB_uncorrected, 0, True, False, (args.render_img is not None), args, s_colorspace, t_colorspace)
 
 if __name__=='__main__':
     main()
