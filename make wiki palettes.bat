48e2143f391fe272e7ec2f365ce5dd9713964641
Persune <gumball2415@gmail.com>
2024-08-07 14:12:13 +0800

    Add 2C03 reference palette

--- make wiki palettes.bat
+++ make wiki palettes.bat
@@ -1,4 +1,6 @@
-palgen_persune.py --skip-plot -phs -5.0 -o docs/NESDev/2C02G_wiki -f ".txt MediaWiki"
-palgen_persune.py --skip-plot -phs -5.0 -o docs/NESDev/2C02G_wiki
-palgen_persune.py --skip-plot -ppu "2C07" -o docs/NESDev/2C07_wiki -f ".txt MediaWiki"
-palgen_persune.py --skip-plot -ppu "2C07" -o docs/NESDev/2C07_wiki
+palgen_persune.py --skip-plot -phs -5.0 -e -o docs/NESDev/2C02G_wiki -f ".txt MediaWiki"
+palgen_persune.py --skip-plot -phs -5.0 -e -o docs/NESDev/2C02G_wiki
+palgen_persune.py --skip-plot -ppu "2C07" -e -o docs/NESDev/2C07_wiki -f ".txt MediaWiki"
+palgen_persune.py --skip-plot -ppu "2C07" -e -o docs/NESDev/2C07_wiki
+palgen_persune.py --skip-plot -ppu "2C03" -e -o docs/NESDev/2C03_wiki -f ".txt MediaWiki"
+palgen_persune.py --skip-plot -ppu "2C03" -e -o docs/NESDev/2C03_wiki
